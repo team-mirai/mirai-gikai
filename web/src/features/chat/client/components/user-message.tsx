404b82b0400ff80610d91ef78cc5b5a87f6f5649
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-06-26 15:40:05 +0900

    miss
    Signed-off-by: Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>

--- web/src/features/chat/client/components/user-message.tsx
+++ web/src/features/chat/client/components/user-message.tsx
@@ -12,11 +12,12 @@ export function UserMessage({ message }: UserMessageProps) {
         variant="flat"
         className="max-w-fit text-sm font-medium leading-[2] text-black bg-mirai-gradient rounded-2xl px-4 !py-0.5"
       >
-        {message.parts.map((part) => {
+        {message.parts.map((part, i) => {
           if (part.type === "text") {
             return (
               <span
-                key={`${message.id}-${part.text}`}
+                // biome-ignore lint/suspicious/noArrayIndexKey: index以外のユニークなキーはない
+                key={`${message.id}-${i}`}
                 className="whitespace-pre-wrap break-words"
               >
                 {part.text}
